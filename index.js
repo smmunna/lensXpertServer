8ece8334c02ced4ae243181b88e5f2f2051b015a
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-06-12 15:27:33 +0600

    updated

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const verifyJWT = (req, res, next) => {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const database = client.db("lensxpertDB");
         const classesCollection = database.collection("classes");
