8e2b0763228f57785464df57a7bebddc044b9484
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-06-12 15:06:19 +0600

    updated

--- index.js
+++ index.js
@@ -144,18 +144,8 @@ async function run() {
 
 
         // Getting the instrutor classes based on the maill
-        app.get('/classes/myclasses', verifyJWT, async (req, res) => {
-            // console.log(req.headers.authorization)
-            const decode = req.decode
-            // console.log('Comeback after decode',decode.user)
-            if (decode.user !== req.query.email) {
-                res.status(403).send("Unauthorized access")
-            }
-            let query = {}
-            if (req.query?.email) {
+        app.get('/classes/myclasses', async (req, res) => {
                 query = { instructorEmail: req.query.email }
-            }
-
             const result = await classesCollection.find(query).toArray()
             res.send(result)
         })
